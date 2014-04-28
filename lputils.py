245b7a7d5727555c752b60a95b8c00f7b7771479
Estefi <estefany@loadingplay.com>
2014-04-28 14:20:59 -0300

    Formato moneda

--- lputils.py
+++ lputils.py
@@ -4,13 +4,16 @@
 ## transform a number to money format
 def MoneyFormat(number):
 
-	str_number = "$ "
+	# str_number = "$ "
 
-	counter = 0
-	for x in str(number):
-		if counter % 3 == 0 and counter != 0:
-			str_number += ""
-		str_number += x
-		counter += 1
+	# counter = 0
+	# for x in str(number):
+	# 	if counter % 3 == 0 and counter != 0:
+	# 		str_number += "."
+	# 	#str_number += x
+	# 	counter += 1
 
-	return "{}".format(str_number)
+	# return "{}".format(str_number)
+
+	import math
+	return '$' + str(format(math.floor(number * 100) / 100, ',.0f'))
