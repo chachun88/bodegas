a416301ebb5b849ad4e837d31a88068255285774
Estefi <estefany@loadingplay.com>
2014-05-22 18:27:10 -0300

    informe con filtros

--- report_handler.py
+++ report_handler.py
@@ -15,8 +15,14 @@ from model.product import Product
 class ReportHandler(BaseHandler):
 	def get(self):
 		self.set_active(Menu.INFORMES_POR_BODEGA)
+
+		try:
+			day = self.get_argument("day")
+		except:
+			day="today"	
+		
 		# data = Cellar().List(1, 10)
 		cellar = Cellar().List(1, 10)
-		data = Cellar().ListKardex()
+		data = Cellar().ListKardex(day)
 		product = Product().get_product_list()
 		self.render("report/home.html", side_menu=self.side_menu, data=data, product=product, cellar=cellar)
