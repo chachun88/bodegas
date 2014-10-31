06c504afe55e0e334cd9a6cb239662c3a38da51a
Yi Chun <yichun212@gmail.com>
2014-10-31 17:23:53 -0300

    ordenar detalle bodega por sku

--- webservice_rest/model10/cellar.py
+++ webservice_rest/model10/cellar.py
@@ -511,7 +511,7 @@ class Cellar(BaseModel):
 
         cur = self.connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-        query = '''select distinct product_sku, size from "Kardex" where cellar_id = %(id)s'''
+        query = '''select distinct product_sku, size from "Kardex" where cellar_id = %(id)s order by product_sku'''
         parametros = {
         "id":self.id
         }
