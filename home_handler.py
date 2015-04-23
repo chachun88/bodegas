564f40c41cd7409ec4ae15426761d746382fdc79
Yi Chun <yichun212@gmail.com>
2015-04-23 18:03:51 -0300

    no message

--- home_handler.py
+++ home_handler.py
@@ -475,8 +475,6 @@ class ProductMassiveOutputHandler(BaseHandler):
 
                                 if "error" in res_remove:
                                     warnings.append(res_remove['error'].encode("utf-8"))
-                            else:
-                                warnings.append('''Debe especificar cantidad y precio de salida para el producto {}'''.format(sku))
                         else:
                             warnings.append('''No se reconoce la bodega '{}', Tip: vaya a "stock/todas las bodegas",
                               y copie el nombre de la bodega a la cual desea relacionar el movimiento de stock, 
