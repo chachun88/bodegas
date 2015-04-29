4a2aeff756b8cb373e359779f0903484d73dcc03
Yi Chun <yichun212@gmail.com>
2015-04-29 12:02:11 -0300

    [TEST] move to another cellar

--- static/js/easy.js
+++ static/js/easy.js
@@ -56,9 +56,38 @@ $(document).ready(function() {
         }
     });
 
+    $("#movimiento select#sku").change(function() {
+
+        var json_string = $("#movimiento input#product_list").val()
+        obj = $.parseJSON(json_string);
+
+        for (i = 0; i < obj.length; i++) {
+
+            if (obj[i]["sku"] == $(this).val()) {
+
+                $("#movimiento select#size").empty();
+
+                var sizes = obj[i]["size"].split(",");
+
+                $("#movimiento select#size").append($('<option>', {
+                        value: '',
+                        text: 'Seleccione'
+                }));
+
+                for (j = 0; j < sizes.length; j++) {
+                    $("#movimiento select#size").append($('<option>', {
+                        value: sizes[j],
+                        text: sizes[j]
+                    }));
+                }
+            }
+        }
+    });
+
     $(".form-easy").submit(function(event) {
         // agregar productos a bodega
         var cellar_id = $("select[name=cellar_id]", $(this)).val();
+        var new_cellar = $("select[name=new_cellar]", $(this)).val();
         var product_sku = $("select[name=sku]", $(this)).val();
         var quantity = $("input[name=quantity]", $(this)).val();
         var price = $("input[name=price]", $(this)).val();
@@ -67,6 +96,7 @@ $(document).ready(function() {
 
         var post_data = {
             "cellar_id": cellar_id,
+            "new_cellar": new_cellar,
             "sku": product_sku,
             "quantity": quantity,
             "price": price,
