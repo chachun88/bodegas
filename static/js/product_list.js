24bd0d5bb35c764194f63520381984b270aec644
Yi Chun Lin <yi@loadingplay.com>
2015-06-11 16:34:43 -0300

    cambiar configuración wysiwyg

--- static/js/product_list.js
+++ static/js/product_list.js
@@ -82,13 +82,13 @@ var FastEdit = function(form_id){
             }
         }
     });
-}
+};
 
 $(document).ready(function(){
     $('#fastedit-textarea').wysihtml5({
         "font-styles": false, //Font styling, e.g. h1, h2, etc. Default true
         "emphasis": true, //Italics, bold, etc. Default true
-        "lists": false, //(Un)ordered lists, e.g. Bullets, Numbers. Default true
+        "lists": true, //(Un)ordered lists, e.g. Bullets, Numbers. Default true
         "html": false, //Button which allows you to edit the generated HTML. Default false
         "link": false, //Button to insert a link. Default true
         "image": false, //Button to insert an image. Default true,
