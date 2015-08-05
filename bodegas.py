c5e187d8e41789b250b3562bb41a696eb894507d
Yi Chun Lin <yi@loadingplay.com>
2015-08-04 22:34:38 -0300

    change static version

--- bodegas.py
+++ bodegas.py
@@ -36,7 +36,7 @@ from config import *
 
 # something
 define("port", default=PORT, help="run on the given port", type=int)
-define("nocache_static", default="static_v33", help="", type=str)
+define("nocache_static", default="static_v34", help="", type=str)
 define("upload", default="upload", help="", type=str)
 
 if "enviroment" not in options:
