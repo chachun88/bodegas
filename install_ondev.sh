8e47904cc71b793c148ebe65bb147a14541d5397
ricardo <ricardo@loadingplay.com>
2016-03-02 14:38:37 -0300

    install modified

--- install_ondev.sh
+++ install_ondev.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-LOCALREPO_VC_DIR=/var/www/sites.ondev/.git
+LOCALREPO_VC_DIR=/var/www/bgiani.ondev/.git
 
 if [ ! -d $LOCALREPO_VC_DIR ]
 then
@@ -13,7 +13,6 @@ cd /var/www/sites.ondev
 unset GIT_DIR
 
 echo "checkout"
-git checkout origin/ondev_master -f
 git checkout ondev_master -f
 
 echo "pull"
@@ -28,4 +27,3 @@ bower install
 
 echo "change permissions"
 chmod +x install_ondev.sh
-
