d7a1d9d3a677618f913b63c6f7781df5708cb2b7
ricardo <ricardo@loadingplay.com>
2016-03-02 15:11:43 -0300

    addded submodule dafiti

--- install_ondev.sh
+++ install_ondev.sh
@@ -19,6 +19,7 @@ echo "pull"
 git pull
 
 echo "init submodule"
+git config submodule.dafiti.url http://ondev:loadingplay007@gogs.ondev.today/loadingplay/dafiti.git
 git submodule update --init --recursive
 
 echo "npm and bower"
