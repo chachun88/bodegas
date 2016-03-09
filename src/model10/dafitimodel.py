411742a5e831ca34efcbf837c6186dd9a1011a4d
ricardo <ricardo@loadingplay.com>
2016-03-09 14:23:16 -0300

    images with upper case

--- src/model10/dafitimodel.py
+++ src/model10/dafitimodel.py
@@ -121,7 +121,7 @@ class DafitiModel(BaseModel):
 
         for img in images:
             if img != '':
-                final_images.append("http://bodegas.gianidafirenze.cl/image/dafiti/{}?mwh=1380,1160".format(img))
+                final_images.append("http://bodegas.gianidafirenze.cl/image/dafiti/{}?mwh=1380,1160".format(img.upper())
 
         # adding images to dafiti
         self.client.product.Image(
